40c95d07bfbfbf7e1bb15c5896510957fd32a6b7
Gilad Gray <giladgray@gmail.com>
2022-07-25 11:04:07 -0700

    add stack of incoming tiles

--- src/HexGrid.tsx
+++ src/HexGrid.tsx
@@ -19,7 +19,8 @@ interface MyHex extends Hex {
   type: TerrainType;
 }
 
-const HEX = createHexPrototype<MyHex>({ dimensions: 30, type: TerrainType.PLAIN });
+const SIZE = 30;
+const HEX = createHexPrototype<MyHex>({ dimensions: SIZE, type: TerrainType.PLAIN });
 
 const DIRECTIONS = [
   // CompassDirection.N,
@@ -51,8 +52,16 @@ const Hexy = {
   },
 };
 
+/** Create a number of new tiles. */
+function createTiles(count = 10) {
+  return Array(count)
+    .fill(TerrainType.PLAIN, 0)
+    .map(() => Math.floor(Math.random() * 4) as TerrainType);
+}
+
 export const HexGrid: React.FC = () => {
   const [grid, setGrid] = useState(Hexy.create);
+  const [stack, setStack] = useState(createTiles);
 
   // get all hexes from grid.store
   const hexes: MyHex[] = [];
@@ -71,8 +80,10 @@ export const HexGrid: React.FC = () => {
   const handleClick: React.MouseEventHandler = ({ nativeEvent: { offsetX, offsetY } }) => {
     const pt = grid.pointToHex({ x: offsetX, y: offsetY });
     // can place in empty tile next to a placed tile
-    if (!Hexy.has(grid, pt) && neighbors.has(pt.toString())) {
-      pt.type = Math.floor(Math.random() * 4);
+    if (stack.length > 0 && !Hexy.has(grid, pt) && neighbors.has(pt.toString())) {
+      const [head, ...rest] = stack;
+      pt.type = head;
+      setStack(rest);
       setGrid(
         grid.update(g => {
           g.store.set(pt.toString(), pt);
@@ -96,9 +107,23 @@ export const HexGrid: React.FC = () => {
         {hexes.map((hex, i) => (
           <polygon key={`tile-${i}`} points={Hexy.points(hex)} fill={TERRAIN_COLORS[hex.type]} />
         ))}
-        {Array.from(neighbors.values()).map((hex, i) => (
-          <polygon key={`empty-${i}`} points={Hexy.points(hex)} className="open" stroke="lightblue" fillOpacity={0} />
-        ))}
+        {stack.length > 0 &&
+          Array.from(neighbors.values()).map((hex, i) => (
+            <polygon key={`empty-${i}`} points={Hexy.points(hex)} className="open" stroke="lightblue" fill="snow" />
+          ))}
+      </svg>
+      <svg width={SIZE * 2} height={height}>
+        {stack
+          .map((t, i) => (
+            <g key={`stack-${i}`} transform={`translate(${SIZE}, ${height - SIZE / 3 - (stack.length - i) * 10})`}>
+              <polygon points={Hexy.points(stackTile)} stroke="white" strokeWidth={3} fill={TERRAIN_COLORS[t]} />
+            </g>
+          ))
+          .reverse()}
+        <g transform={`translate(${SIZE}, ${height - SIZE / 3})`}>
+          <rect fill="white" rx={6} x={-12} y={-15} width={24} height={20} />
+          <text textAnchor="middle">{stack.length}</text>
+        </g>
       </svg>
     </div>
   );
@@ -116,7 +141,7 @@ ROADMAP
   - desert?
   - tundra?
   - swamp?
-- stack of incoming tiles
+x stack of incoming tiles
 - draw percentage of terrain types
 - each tile edge has terrain type
 - rotate incoming tiles
