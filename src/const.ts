79e3adfbf0464e49e8ef96831f260aed63cf1544
Gilad Gray <gilad@peregrine.io>
2022-08-08 18:35:33 -0700

    add DESERT terrain

--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,14 @@
 export enum TerrainType {
   PLAIN,
+  DESERT,
   FOREST,
   MOUNTAIN,
   WATER,
 }
 
 export const TERRAIN_COLORS: Record<TerrainType, string> = {
-  [TerrainType.PLAIN]: '#E1C16E',
+  [TerrainType.PLAIN]: '#d71868',
+  [TerrainType.DESERT]: '#E1C16E',
   [TerrainType.FOREST]: '#228B22',
   [TerrainType.MOUNTAIN]: '#696969',
   [TerrainType.WATER]: '#0096FF',
